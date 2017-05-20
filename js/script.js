90e24e487679c071e2703451b1c7f895eaadfa76
Salim Djerbouh <tbitw31@gmail.com>
2017-05-20 20:45:45 +0100

    JS switch Signed-off-by: Salim Djerbouh <tbitw31@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-document.write("hello");
+var age = 6;
+switch (age) {
+  case 6 :
+    document.write("Go to kindergarden");
+    break;
+  case 10 :
+    document.write("Go to primary school");
+  case 15 :
+    document.write("Go to middle school");
+  default :
+    document.write("Go somewhere else");
+}
