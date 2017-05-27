a53f09b74101bf30c5f4a2381bdb2db07ce53d37
Salim Djerbouh <tbitw31@gmail.com>
2017-05-27 19:58:00 +0100

    Ternary Operator :squirrel: Signed-off-by: Salim Djerbouh <tbitw31@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,12 +1,4 @@
 var age = 6;
-switch (age) {
-  case 6 :
-    document.write("Go to kindergarden");
-    break;
-  case 10 :
-    document.write("Go to primary school");
-  case 15 :
-    document.write("Go to middle school");
-  default :
-    document.write("Go somewhere else");
-}
+// Ternary operator
+var canIVote = (age >= 18) ? true : false;
+document.write(canIVote);
