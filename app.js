322e857f5801be86de5b7065fcd5c3209712498c
Caddy Dz <tbitw31@gmail.com>
2017-11-14 23:21:29 +0100

    VueJS Part3 methods
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 new Vue({
   el: '#root',
   data: {
-    name: 'Salim'
+    name: 'Salim',
+    job: 'Developer'
+  },
+  methods: {
+    greet: function(time) {
+      return 'Good ' + time + ' ' + this.name;
+    }
   }
 });
