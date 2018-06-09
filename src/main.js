f0ae31987e8c5fd075373dc8cab7745f466d15e1
Salim Djerbouh <github@caddydz.me>
2018-06-09 10:46:57 +0100

    Nesting components example
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-// Importing components globally
-// import Ninjas from './Ninjas.vue'
-
-// Vue.component('ninjas', Ninjas);
 
 Vue.config.productionTip = false
 
