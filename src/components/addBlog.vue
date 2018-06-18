9003ac3bc8265611179e296e2d3e8e34c9c08832
Salim Djerbouh <github@caddydz.me>
2018-06-18 14:03:01 +0100

    HTTP Requests
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="add-blog">
 		<h2>Add a New Blog Post</h2>
-		<form>
+		<form v-show="!submitted">
 			<label>Blog Title:</label>
 			<input type="text" v-model.lazy="blog.title" required>
 			<label></label>
@@ -22,7 +22,11 @@
 					{{ author }}
 				</option>
 			</select>
+			<button @click.prevent="post">Add Blog</button>
 		</form>
+		<div v-show="submitted">
+			<h3>Thanks for adding your post</h3>
+		</div>
 		<div id="preview">
 			<h3>Preview Blog</h3>
 			<p>Blog title: {{ blog.title }}</p>
@@ -49,11 +53,21 @@ export default {
 				categories: [],
 				author: ''
 			},
-			authors: ['The Net Ninja', 'The Angular Avenger', 'The Vue Vindicator']
+			authors: ['The Net Ninja', 'The Angular Avenger', 'The Vue Vindicator'],
+			submitted: false
     }
   },
   methods: {
-
+		post() {
+			this.$http.post('http://jsonplaceholder.typicode.com/posts', {
+				title: this.blog.title,
+				body: this.blog.content,
+				userId: 1
+			}).then(function(data) {
+				console.log(data);
+				this.submitted = true;
+			});
+		}
   }
 }
 </script>
