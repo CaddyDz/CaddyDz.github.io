cb257997d2760d5478480325e9fab1293c0d8e4b
Salim Djerbouh <github@caddydz.me>
2018-06-25 15:34:04 +0100

    Posting to firebase
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -59,11 +59,7 @@ export default {
   },
   methods: {
 		post() {
-			this.$http.post('http://jsonplaceholder.typicode.com/posts', {
-				title: this.blog.title,
-				body: this.blog.content,
-				userId: 1
-			}).then(function(data) {
+			this.$http.post('https://fir-7ed63.firebaseio.com/posts.json', this.blog).then(function(data) {
 				console.log(data);
 				this.submitted = true;
 			});
