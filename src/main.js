50bb1bdf479f05893f0530e3a9202a747575e4b6
Salim Djerbouh <github@caddydz.me>
2018-06-26 13:24:17 +0100

    Arrows
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- src/main.js
+++ src/main.js
@@ -1,15 +1,5 @@
-// let
-// const
-// var
+let names = ['Taylor', 'Jeffrey', 'Adam', 'Matt'];
 
-// let prevents the moisting undefined error for var assignment
+names = names.map(name => `${name} is cool`);
 
-const months = ['January', 'February'];
-months.push('bam');
-console.log(months);
-
-// You can force immutability with
-const Immutablemonths = Object.freeze(['January', 'February']);
-Immutablemonths.push('March');
-
-console.log(Immutablemonths); // Cannot add property 2, object is not extensible
+console.log(names);
